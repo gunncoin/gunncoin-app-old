fbd9e34a6cc1cb801dbabd3746248c2e7ac51f89
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2022-02-06 01:28:31 -0800

    more wallet stuff

--- screens/WalletScreen.tsx
+++ screens/WalletScreen.tsx
@@ -6,20 +6,28 @@ import {
   Heading,
   HStack,
   VStack,
+  Text,
 } from "native-base";
 import { View } from "../components/Themed";
 
 export const WalletScreen = () => {
   return (
-    <Center px={3}>
-      <Heading size="xl" color="white">
-        {"$100.00"}
-      </Heading>
-      <Heading size="m">{"95012345"}</Heading>
-      <HStack space={3} justifyContent={"center"}>
-        <Button>Send</Button>
-        <Button>Receive</Button>
-      </HStack>
+    <Center px={3} height="100%" width="100%">
+      <Center bg="gray.700" rounded="md" width="100%" paddingTop={30}>
+        <Heading size="xl" color="white">
+          {"$100.00"}
+        </Heading>
+        <HStack
+          space={3}
+          justifyContent={"center"}
+          marginTop={5}
+          marginBottom={15}
+        >
+          <Button rounded={"sm"}>Send</Button>
+          <Button rounded={"sm"}>Receive</Button>
+        </HStack>
+        <Text color={"gray.100"}>{"95030486"}</Text>
+      </Center>
     </Center>
   );
 };
