917b60fc6d4541ada844af11badca01360e8edc0
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2022-02-06 02:58:49 -0800

    start game screen

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,35 +1,12 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "../components/EditScreenInfo";
+import { Center, Heading } from "native-base";
 import { Text, View } from "../components/Themed";
 
-export default function TabTwoScreen() {
+export const GameScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
+    <Center height={"100%"} width="100%">
+      <Heading>{"Deadline $DD:MM:YY"}</Heading>
+    </Center>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
+export default GameScreen;
