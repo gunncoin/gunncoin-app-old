2936a15236b31405f54c11e266faa34efa3ce9ee
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2022-02-05 23:49:29 -0800

    ash you can start working on this now

---
+++ screens/SettingScreen.tsx
@@ -0,0 +1,11 @@
+import { Box, Center, Container } from "native-base";
+
+export const SettingScreen = () => {
+  return (
+    <Center>
+      <Container></Container>
+    </Center>
+  );
+};
+
+export default SettingScreen;
---
+++ screens/WalletScreen.tsx
@@ -0,0 +1,12 @@
+import { Box, Center, Container } from "native-base";
+import { View } from "../components/Themed";
+
+export const WalletScreen = () => {
+  return (
+    <Center>
+      <Container>HELLO</Container>
+    </Center>
+  );
+};
+
+export default WalletScreen;
